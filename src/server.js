4296284d8941e13f465f25fdf31b85a4e948252d
Anderson <tornadotutoriais@gmail.com>
2022-07-21 18:15:50 -0300

    100%

--- src/server.js
+++ src/server.js
@@ -31,5 +31,5 @@ server.use((req, res)=>{
 })
 
 server.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log('Server running...')
   });

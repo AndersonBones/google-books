457e0895161ef3d781ba8973136e0dea2615cf99
Anderson Bones <91613692+AndersonBones@users.noreply.github.com>
2024-01-26 22:48:21 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: process.env.URL,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
